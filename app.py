7d5b25813478057a93e45cf6599d30373fe4bc3f
luu <luciagruiz9@gmail.com>
2024-04-30 09:01:42 +0200

    Programa Base sin formato

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, abort, redirect, request
+from flask import Flask, render_template, abort, redirect, request, url_for
 import json
 import os
 
@@ -12,5 +12,31 @@ with open("static/data/nba.json") as n:
 def inicio():
     return render_template("index.html")
 
+@app.route('/equipos', methods=['GET', 'POST'])
+def equipos():
+    if request.method == 'POST':
+        busqueda = request.form.get('busqueda', '').lower()
+        if busqueda:
+            equipos_encontrados = [equipo for equipo in datos if equipo['teamName'].lower().startswith(busqueda)]
+        else:
+            equipos_encontrados = datos
+        return redirect(url_for('listaequipos', equipos=json.dumps(equipos_encontrados)))
+    return render_template("equipos.html")
 
-app.run("0.0.0.0",port,debug=True)
+@app.route('/listaequipos', methods=['GET', 'POST'])
+def listaequipos():
+    equipos = json.loads(request.args.get('equipos'))
+    return render_template("listaequipos.html", equipos=equipos)
+
+@app.route('/equipo/<int:id>')
+@app.route('/equipo')
+def equipo(id=None):
+    if id is None:
+        id = request.args.get('id')
+    equipo_encontrado = next((equipo for equipo in datos if equipo['teamId'] == id), None)
+    if equipo_encontrado:
+        return render_template("equipo.html", equipo=equipo_encontrado)
+    abort(404)
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port, debug=True)
