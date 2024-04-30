59b39512d1d5758cb07a184743f454f8b92b6f6b
luu <luciagruiz9@gmail.com>
2024-04-30 10:05:41 +0200

    Arreglado Formato + Funcionalidades

--- app.py
+++ app.py
@@ -18,20 +18,30 @@ def equipos():
     busqueda = request.form.get('busqueda', '').lower()
     criterio = request.form.get('criterio', '')
 
+    equipos_encontrados = []
+
     # Filtrar equipos según la búsqueda y el criterio
-    if busqueda:
+    if busqueda and criterio:
+        equipos_encontrados = [equipo for equipo in datos if equipo['teamName'].lower().startswith(busqueda) and equipo['conference'] == criterio]
+    elif busqueda:
         equipos_encontrados = [equipo for equipo in datos if equipo['teamName'].lower().startswith(busqueda)]
     elif criterio:
         equipos_encontrados = [equipo for equipo in datos if equipo['conference'] == criterio]
+    
+    # Verificar si no se encontraron equipos
+    if not equipos_encontrados:
+        mensaje = "No se encontraron equipos que coincidan con los criterios de búsqueda."
     else:
-        equipos_encontrados = datos
+        mensaje = None
 
     # Si se envió un formulario, mostrar la lista de equipos encontrados
     if request.method == 'POST':
-        return render_template("equipos.html", equipos=equipos_encontrados, busqueda=busqueda, criterio=criterio)
+        return render_template("equipos.html", equipos=equipos_encontrados, busqueda=busqueda, criterio=criterio, mensaje=mensaje)
 
     # Si es una solicitud GET, mostrar el formulario vacío
-    return render_template("equipos.html", equipos=datos)
+    return render_template("equipos.html", equipos=datos, mensaje=mensaje)
+
+
 
 @app.route('/listaequipos', methods=['GET', 'POST'])
 def listaequipos():
@@ -46,7 +56,9 @@ def equipo(id=None):
     equipo_encontrado = next((equipo for equipo in datos if equipo['teamId'] == id), None)
     if equipo_encontrado:
         return render_template("equipo.html", equipo=equipo_encontrado)
-    abort(404)
+    else:
+        mensaje = "El equipo no fue encontrado."
+        return render_template("error404.html", mensaje=mensaje), 404
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port, debug=True)
